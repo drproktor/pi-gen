4353770c620c3e6a99ae0a377b1c66f5ec8e37a9
Max Huber <maxh@mailbox.org>
2021-02-01 16:23:00 +0100

    Add default OLA universe

--- stage2/40-ola-tweaks/01-run.sh
+++ stage2/40-ola-tweaks/01-run.sh
@@ -8,6 +8,9 @@ EOF
 # Copy OLA plugin config
 install -d "${ROOTFS_DIR}/etc/ola/"
 install -m 664 files/ola-uartdmx.conf "${ROOTFS_DIR}/etc/ola/"
+# Default universe
+install -m 664 files/ola-port.conf "${ROOTFS_DIR}/etc/ola/"
+install -m 664 files/ola-universe.conf "${ROOTFS_DIR}/etc/ola/"
 on_chroot << EOF
 	chown -R olad:olad /etc/ola
 EOF
